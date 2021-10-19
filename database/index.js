224943d095e639480f759f27497a3ea8f0af1260
Christina Nathan <christina.a.nate@gmail.com>
2021-10-19 12:47:23 -0700

    mongoose connection and questions database initialized

--- database/index.js
+++ database/index.js
@@ -1,1 +1,39 @@
 //create a database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/questions', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('db connection successful!');
+});
+
+let qaSchema = new mongoose.Schema({
+  productId: Number,
+  questions: [{
+    questionId: Number,
+    questionBody: String,
+    questionDate: Date,
+    askerName: String,
+    askerEmail: String,
+    helpfulness: Number,
+    reported: Number,
+    answers: [{
+      answerId: Number,
+      answerBody: String,
+      answerDate: Date,
+      answererName: String,
+      answererEmail: String,
+      helpfulness: Number,
+      reported: Number,
+      photos: [{
+        photoId: Number,
+        url: String
+      }]
+    }]
+  }]
+});
+
+const Question = mongoose.model('Question', qaSchema);
+
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('../database/index.js');
 const port = 3030;
 
 app.get('/', (req, res) => {
