ed71a065870dc4284e2178740563ff9bfa0ceee9
Christina Nathan <christina.a.nate@gmail.com>
2021-10-29 11:59:55 -0700

    reported answers are not returned with get answers request, fixed the date

--- server/models/answer.js
+++ server/models/answer.js
@@ -16,23 +16,25 @@ const getAnswersForQuestion = (question_id, page, count, callback) => {
       // console.log('answers: ', response);
       //response is an array of answer objects for a question
       response.forEach((answerObj) => {
-        const { answerId, answerBody, answerDate, answererName, helpfulness, photos } = answerObj;
-        let finalPhotos = photos.map((photo) => {
-          return {
-            id: photo.photoId,
-            url: photo.url
+        const { answerId, answerBody, answerDate, answererName, helpfulness, photos, reported } = answerObj;
+        if (reported === 0) {
+          let finalPhotos = photos.map((photo) => {
+            return {
+              id: photo.photoId,
+              url: photo.url
+            };
+          });
+          // console.log('photos: ', photos);
+          let answer = {
+            answer_id: answerId,
+            body: answerBody,
+            date: answerDate,
+            answerer_name: answererName,
+            helpfulness: helpfulness,
+            photos: finalPhotos
           };
-        });
-        // console.log('photos: ', photos);
-        let answer = {
-          answer_id: answerId,
-          body: answerBody,
-          date: JSON.stringify(answerDate),
-          answerer_name: answererName,
-          helpfulness: helpfulness,
-          photos: finalPhotos
-        };
-        finalResponse.results.push(answer);
+          finalResponse.results.push(answer);
+        }
       });
       // console.log('finalanswers: ', finalResponse);
       // console.log('finalphotos: ', finalResponse.results[0].photos);
