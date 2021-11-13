8c611594a84d33298504fb4bba620f07162b638d
Christina Nathan <christina.a.nate@gmail.com>
2021-11-12 16:24:35 -0800

    added loader.txt

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const app = require('./app.js');
 const db = require('../database/index.js');
 const { importQuestions, importAnswers, importPhotosIntoAnswer } = require('../database/import.js');
+const path = require('path');
 const port = 3030;
 
 // importQuestions(); //45 minutes, 3,518,963 questions
 // importAnswers();
 // importPhotosIntoAnswer();
+
+app.get('/loaderio-11a92392c832e65d8ec9d3b9bb96374c/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'loader.txt'));
+});
 
 app.listen(port, () => {
   console.log(`Q&A Servive listening at http://localhost:${port}`);
