b4237a3fd0a293d6acf6a117d95eef663d83513b
Christina Nathan <christina.a.nate@gmail.com>
2021-11-19 20:09:20 -0800

    updated get questions

--- server/models/question.js
+++ server/models/question.js
@@ -12,8 +12,6 @@ const getQuestionsForProduct = (product_id, page = 1, count = 100, callback) => 
     results: []
   };
 
-  counter = 0;
-
   Question.find({ productId: Number(product_id), reported: 0 }).hint({ productId: 1 }).limit(count)
     .then((response) => {
       // console.log(response);
@@ -31,7 +29,7 @@ const getQuestionsForProduct = (product_id, page = 1, count = 100, callback) => 
 
       Promise.all(pending)
         .then(answers => {
-          console.log('ANSWERS: ', answers);
+          // console.log('ANSWERS: ', answers);
           response.forEach(questionObj => {
 
             const { questionId, questionBody, questionDate, askerName, askerEmail, reported, helpfulness } = questionObj;
@@ -76,7 +74,7 @@ const getQuestionsForProduct = (product_id, page = 1, count = 100, callback) => 
           callback(null, finalResponse);
         })
         .catch(err => {
-          console.log('err: ', err);
+          callback(err);
         });
     })
     .catch((err) => {
@@ -86,28 +84,28 @@ const getQuestionsForProduct = (product_id, page = 1, count = 100, callback) => 
 };
 
 //getAnswersForQuestion in this one needs to return urls in photo array, currently returns id and url.
-const getFinalAnswersPromise = (questionId, page, count, finalAnswers = {}) => {
-  return new Promise((resolve, reject) => {
-    getAnswersForQuestion(questionId, page, count, (err, answersObj) => {
-      let answersCollection = answersObj.results;
-      answersCollection.forEach(answer => {
-        let photosWithUrlsOnly = answer.photos.map(photo => photo.url);
-        let answerWithId = {
-          id: answer.answer_id,
-          body: answer.body,
-          date: answer.date,
-          answerer_name: answer.answerer_name,
-          helpfulness: answer.helpfulness,
-          photos: photosWithUrlsOnly
-        }
-        // delete Object.assign(answerWithId, answer, { id: answer.answer_id }).answer_id;
-        finalAnswers[answerWithId.id] = answerWithId;
-      });
-      // console.log('final answersssss: ', finalAnswers);
-      resolve(finalAnswers);
-    });
-  })
-}
+// const getFinalAnswersPromise = (questionId, page, count, finalAnswers = {}) => {
+//   return new Promise((resolve, reject) => {
+//     getAnswersForQuestion(questionId, page, count, (err, answersObj) => {
+//       let answersCollection = answersObj.results;
+//       answersCollection.forEach(answer => {
+//         let photosWithUrlsOnly = answer.photos.map(photo => photo.url);
+//         let answerWithId = {
+//           id: answer.answer_id,
+//           body: answer.body,
+//           date: answer.date,
+//           answerer_name: answer.answerer_name,
+//           helpfulness: answer.helpfulness,
+//           photos: photosWithUrlsOnly
+//         }
+//         // delete Object.assign(answerWithId, answer, { id: answer.answer_id }).answer_id;
+//         finalAnswers[answerWithId.id] = answerWithId;
+//       });
+//       // console.log('final answersssss: ', finalAnswers);
+//       resolve(finalAnswers);
+//     });
+//   })
+// }
 
 const createQuestionForProduct = (body, name, email, product_id, callback, reported = 0, helpfulness = 0) => {
   // console.log('createQuestionForProduct invoked!');
